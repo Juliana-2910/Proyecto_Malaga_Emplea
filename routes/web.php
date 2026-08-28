e562b4bdbd5ccbda5e4d69aa2caefa61c2b1e8d6
Juliana Alvarez <juliana.loal29@gmail.com>
2026-08-28 15:42:50 -0500

    Revert " CRUD de la tabla Roles"

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RolesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::resource('roles', RolesController::class);
